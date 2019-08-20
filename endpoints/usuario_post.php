390e8f556bd5a0a30af52c20f78604fefbfecc95
[Vitor Hugo] <silvalimavitorhugo@gmail.com>
2019-08-20 16:41:37 -0300

    Construindo POST de usuário com informações adicionais

--- endpoints/usuario_post.php
+++ endpoints/usuario_post.php
@@ -1,11 +1,44 @@
 <?php
 
 function api_usuario_post($request) {
+  // Recupera os valores do objeto que foi passado na requisição
+  $email = sanitize_email($request['email']);
+  $senha = sanitize_text_field($request['senha']);
+  $nome = sanitize_text_field($request['nome']);
+  $rua = sanitize_text_field($request['rua']);
+  $cep = sanitize_text_field($request['cep']);
+  $numero = sanitize_text_field($request['numero']);
+  $bairro = sanitize_text_field($request['bairro']);
+  $cidade = sanitize_text_field($request['cidade']);
+  $estado = sanitize_text_field($request['estado']);
 
-  $response = array(
-    'nome' => 'Andre',
-    'email' => 'andre@origamid.com'
-  );
+  $user_exists = username_exists($email);
+  $email_exists = email_exists($email);
+
+  // Checa se ja existe um usuário com esse email e se a senha que o usuario passou não está vazia
+  if(!$user_exists && !$email_exists && $email && $senha) {
+    // Insere o usuário
+    $user_id = wp_create_user($email, $senha, $email);
+
+    // Atualiza algumas informações do usuario que acabou de se cadastrar
+    $response = array(
+      'ID' => $user_id,
+      'display_name' => $nome,
+      'first_name' => $nome,
+      'role' => 'subscriber',
+    );
+    wp_update_user($response);
+
+    // Inserindo os novos dados do usuário. Update irá criar o campo se o usuário não tiver esse campo (usuario, campo, valor)
+    update_user_meta($user_id, 'cep', $cep);
+    update_user_meta($user_id, 'rua', $rua);
+    update_user_meta($user_id, 'numero', $numero);
+    update_user_meta($user_id, 'bairro', $bairro);
+    update_user_meta($user_id, 'cidade', $cidade);
+    update_user_meta($user_id, 'estado', $estado);
+  } else {
+    $response = new WP_Error('email', 'Email já cadastrado.', array('status' => 403));
+  }
   return rest_ensure_response($response);
 }
 
